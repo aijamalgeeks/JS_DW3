8afe38280754cb2e3593b6d1c2e0363b71cc17b9
aijamalgeeks <130555516+aijamalgeeks@users.noreply.github.com>
2023-04-14 16:53:28 +0600

    Add files via upload

---
+++ DZ3_Aijamal_25-03/dz3/app.js
@@ -0,0 +1,60 @@
+for (let i=0 ; i<=100; i++){
+    if(i%3==0 && i%5==0){
+        console.log("FizzBuzz")
+    }else if(i%3==0){
+        console.log("Fizz")
+    }else if(i%5==0){
+        console.log("Buzz")
+    }else{
+        console.log(i)
+    }
+}
+
+var predmet = ['milk','beer','beer','milk','milk']
+for (i of predmet) {
+    if (i == 'milk') {
+        console.log('good')
+    } else {
+        console.log('bad')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
